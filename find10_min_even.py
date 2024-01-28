55ed53037e73680cb72cedb5b7302b24cd29a8d7
Abduvaliyevakumushxon <shodmonovd1985@gmail.com>
2024-01-28 20:31:07 +0500

    10-vazifa tugadi

--- find10_min_even.py
+++ find10_min_even.py
@@ -5,5 +5,16 @@ def find_min_even(data):
         data: list of numbers
     returns: minimum even number in the list
     """
-    return 0
+    a=[]
+    for i in data:
+        if i%2==0:
+            a.append(i)
+    if len(a)==0:
+        return -1
+    mn=a[0]
+    for s in a:
+        if mn>s:
+            mn=s
+    return mn 
+print(find_min_even(data=[1, 8, 2, 8, 5]))
 
