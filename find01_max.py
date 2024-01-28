9856905ebf8bff25ee22307b72ff7fe7e995e7c9
Abduvaliyevakumushxon <shodmonovd1985@gmail.com>
2024-01-28 19:41:36 +0500

    1-vazifa tugadi

--- find01_max.py
+++ find01_max.py
@@ -5,5 +5,13 @@ def find_max(data):
         data: list of numbers
     returns: maximum number in the list
     """
-    return 0
+    a=0
+    max3=data[0]
+    while a<len(data):
+        if max3<data[a]:
+            max3=data[a]
+        a+=1
+    return max3
+print(find_max(data=[1,2,3,4,5]))
+
     
