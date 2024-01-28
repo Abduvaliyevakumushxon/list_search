b7c2dc9028be646273c830fc24989db3e21c2638
Abduvaliyevakumushxon <shodmonovd1985@gmail.com>
2024-01-28 21:20:39 +0500

    11-vazifa tugadi

--- find11_min_odd.py
+++ find11_min_odd.py
@@ -5,5 +5,17 @@ def find_min_odd(data):
         data: list of numbers
     returns: minimum odd number in the list
     """
-    return 0
+    s=[]
+    for i in data:
+        if i%2==1:
+            s.append(i)
+    if len(s)==0:
+        return -1
+    mn=s[0]
+    for a in s:
+        if mn>a:
+            mn=a
+    
+    return mn
+print(find_min_odd( [1, 8, 2, 8, 5]))
 
