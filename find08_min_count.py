c7c1d87dd4a3d63103f3c17651f7848b265f1eec
Abduvaliyevakumushxon <shodmonovd1985@gmail.com>
2024-01-28 20:14:26 +0500

    8-vazifa tugadi

--- find08_min_count.py
+++ find08_min_count.py
@@ -5,4 +5,9 @@ def find_min_count(data):
         data: list of numbers
     returns: count of minimum numbers in the list
     """
-    return 0
+    mn=data[0]
+    for i in data:
+        if mn>i:
+            mn=i
+    return data.count(mn) 
+print(find_min_count(data=[1, 8, 3, 8, 5]))
