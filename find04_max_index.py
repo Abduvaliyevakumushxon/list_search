2c36d610e27306ae6a085fa5722d05ee84e9aa56
Abduvaliyevakumushxon <shodmonovd1985@gmail.com>
2024-01-28 19:57:04 +0500

    4-vazifa tugadi

--- find04_max_index.py
+++ find04_max_index.py
@@ -5,4 +5,10 @@ def find_max_index(data):
         data: list of numbers
     returns: index of maximum number in the list
     """
-    return 0
+    mx=data[0]
+    for i in data:
+        if mx<i:
+            mx=i
+
+    return data.index(mx)
+print(find_max_index(data=[1,2,3,4,5]))
