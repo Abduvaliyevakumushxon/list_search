d49a660ce1acfaa568d45f4f4ac6a1a41d76ac58
Abduvaliyevakumushxon <shodmonovd1985@gmail.com>
2024-01-28 20:05:32 +0500

    6-vazifa tugadi

--- find06_max_odd.py
+++ find06_max_odd.py
@@ -5,4 +5,16 @@ def find_max_odd(data):
         data: list of numbers
     returns: maximum odd number in the list
     """
-    return 0
+    a=[]
+    for i in data:
+        if i%2==1:
+            a.append(i)
+    if len(a)==0:
+        return -1
+    mx=a[0]
+    for d in a:
+        if mx<d:
+            mx=d
+    
+    return mx
+print(find_max_odd([11, 8, 3, 8, 5]))
